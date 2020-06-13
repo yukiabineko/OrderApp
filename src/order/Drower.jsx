80e233656905b8e72a23ca0cb2ef5a4702d2ed11
abineko2 <yuki1980426@gmail.com>
2020-06-13 11:06:11 +0900

    テーブルモーダル表示

--- src/order/Drower.jsx
+++ src/order/Drower.jsx
@@ -4,12 +4,33 @@ import { connect } from 'react-redux';
 import { foodData, drinkData } from '../data/Search';
 import DrinkButton from './DrinkButton';
 import FoodButton from './FoodButton';
+import { useState } from 'react';
 
 
 const Drower = (props)=>{
+  const[state, setState] = useState({
+    name: '',
+    price: '',
+  })
   const foods = foodData(props.data);
   const drinks = drinkData(props.data);
+  //ステートに追加
 
+  const addItem = (event)=>{
+    let data = event.target.value;
+    let name = data.split(',')[0];
+    let price = data.split(',')[1];
+    setState({
+      name: name,
+      price: price
+    })
+
+  }
+  //親コンポーネントに送信
+  const sendData = (event)=>{
+    event.preventDefault();
+    props.parentData({name: state.name, price: state.price});
+  }
 
   return(
    <div>
@@ -19,24 +40,24 @@ const Drower = (props)=>{
 
      {/*...軽食selectbox...*/}
 
-     <form className="form-inline bg-light">
+     <form className="form-inline bg-light" onSubmit={sendData}>
        <label className="mr-3 font-weight-bold pt-4 pb-4">軽食:</label>
-       <select className="form-control mr-3 w-50">
+       <select className="form-control mr-3 w-50" onChange={addItem}>
          <option></option>
          {foods.map((value,i)=>(
-           <option key={'food'+i} value={value.name}>{value.name}</option>
+           <option key={'food'+i} value={value.name} value={value.name + ',' + value.price}>{value.name}</option>
          ))}
        </select>
        <input type="submit" value="追加" className="btn btn-primary" />
      </form>
      {/*...飲み物selectbox...*/}
 
-     <form className="form-inline bg-light">
+     <form className="form-inline bg-light" onSubmit={sendData}>
        <label className="mr-3 font-weight-bold pt-4 pb-4">飲物:</label>
-       <select className="form-control mr-3 w-50">
+       <select className="form-control mr-3 w-50" onChange={addItem} >
          <option></option>
          {drinks.map((value,i)=>(
-           <option key={'drink'+i} value={value.name}>{value.name}</option>
+           <option key={'drink'+i} value={value.name + ',' + value.price}>{value.name}</option>
          ))}
        </select>
        <input type="submit" value="追加" className="btn btn-primary" />
@@ -46,8 +67,8 @@ const Drower = (props)=>{
      <div className="border border-top mb-1"></div>
      <p className="h5 mb-5">ボタンで入力</p>
      <div className="row btArea">
-       <div className="col-md-5 border border-left offset-1"><DrinkButton /></div>
-       <div className="col-md-5  border-right border-top border-bottom"><FoodButton /></div>
+       <div className="col-md-6"><DrinkButton /></div>
+       <div className="col-md-6 border-left"><FoodButton /></div>
 
      </div>
 
--- src/order/Main.jsx
+++ src/order/Main.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import './Order.css';
 import { connect } from 'react-redux';
 import Drower from './Drower';
+import { useState } from 'react';
 
 
 const Main = ()=>{
+  const[state, setState] = useState({
+    data: [],
+    items: []
+  });
+  const addData = (data)=>{
+    let sendData = state.data.slice();
+    sendData.push({name: data.name, price: data.price});
+    setState({data: sendData, item: state.items});
+  }
   return(
    <div　className="mt-5">
      <div className="text-center text-dark h1 font-weight-bold mb-5">オーダー詳細</div>
@@ -12,8 +22,39 @@ const Main = ()=>{
      <input type="checkbox" id="ordercheck" />
      <label for="ordercheck" className="text-primary h3" id="oderOpen">㊉New</label>
      <label for="ordercheck" className="text-primary h3" id="orderBack"></label>
-     <div id="orderArea"><Drower /></div>
-     <div id="orderModal"></div>
+     <div id="orderArea"><Drower parentData={addData} /></div>
+     <div id="orderModal">
+       <div className="text-center h4 font-weight-bold mt-2 mb-2">[商品オーダーパネル]</div>
+       <div className="border-top mb-3"></div>
+       <div className="row">
+         <div className="col-md-10 offset-1 table_area">
+            <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th className="bg-primary text-white font-weight-bold w-75">商品名</th>
+                <th className="bg-primary text-white font-weight-bold"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.data.map((value)=>(
+                <tr>
+                <td className="font-weight-bold bg-white"> 
+                  <h5>{value.name}</h5>
+                  <div className="border-top border-primary mb-2"></div>
+                  <h6>価格:{value.price}<span className="text-danger">円</span></h6>
+                </td>
+                <td className="font-weight-bold bg-white"><button className="btn btn-danger btn-block">削除</button></td>
+              </tr>
+              ))}
+            </tbody>
+            </table>
+         </div>
+       </div>
+       <div className="text-center">
+         <button className="btn btn-primary">確定</button>
+       </div>
+     </div>
+
 
      <div className="row">
        <div className="col-md-7 bg-light p-5"></div>
