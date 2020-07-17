4b359f0a54e0bf59237bd1b06b9f916923a5f118
abineko2 <yuki1980426@gmail.com>
2020-07-18 08:01:28 +0900

    pdf調整レイアウト

--- src/accounting/Pdf.js
+++ src/accounting/Pdf.js
@@ -23,45 +23,50 @@ var table_content = {
       }
    };
 
-const data =[
-    {date: "4/01", money: "1000", num: "11"},
-    {date: "4/02", money: "2000", num: "12"},
-    {date: "4/03", money: "3000", num: "13"},
-];
+
+const obj = JSON.parse(localStorage.getItem("dates"));
+const keyArray = Object.keys(obj);                        //キー一覧
+const keyCount = keyArray.length;                         //日付キー数
+const today = new Date();
+const makeDay = today.getFullYear() + "年" + (today.getMonth()+1) + "月" + today.getDate() + "日";
 
 
 var table_head = ['日付','売り上げ','点数'];
 var head_tr = [];
 for( let v of table_head ){
-head_tr.push({
-  text: v,
-  fillColor: '#dddddd',
-  style: ["tableHeader", "tableCell"]
-  });
-};
-table_content["table"]["body"][0] = head_tr;
-var table_body = [];
-for(let i=0; i<data.length; i++){
-table_body.push([data[i].date, data[i].money, data[i].num]);
+  head_tr.push({
+    text: v,
+    fillColor: '#dddddd',
+    style: ["tableHeader", "tableCell"]
+    });
+  };
+  table_content["table"]["body"][0] = head_tr;
+  var table_body = [];
+  for(let i=0; i<keyCount; i++){
+  table_body.push([keyArray[i], obj[keyArray[i]]["uriage"], obj[keyArray[i]]["number"]]);
+  if(i == keyCount-1){
+    table_body.push([keyArray[i], obj[keyArray[i]]["uriage"] + " (未確定)", obj[keyArray[i]]["number"] + " (未確定)"]);
+  }
 }
 
 for( let a of table_body ){
   var tr = [];
   for( let v of a ){
-  tr.push({
-  text: v,
-  fillColor: '#fff',
-  style: ["tableCell"]
-  });
+    tr.push({
+    text: v,
+    fillColor: '#fff',
+    style: ["tableCell"]
+    });
   };
 table_content["table"]["body"].push(tr);
 };
 pdfMake.createPdf(
 {
     content: [{
-    text: String(new Date()) + "テキストも追加できます\n\n"},
+    text: "売り上げ確認表\n\n",fontSize: 35,alignment:'center',bold: 'Courier-Bold',},
+    {text: "作成日　"+ makeDay, alignment:'right'},
     table_content,
-    {text: "\n\nテーブルの後ろに追加してみました。"}
+    {text: "\n計"+ keyCount+ "件",alignment:'right'}
     ],
     defaultStyle:{
     font: 'GenShin'//ここでデフォルトのスタイル名を指定しています。
